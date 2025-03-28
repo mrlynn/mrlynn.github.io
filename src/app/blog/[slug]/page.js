c0577c452fb85e64182a7bc772fe384f5b1ad7d8
Michael Lynn <merlynn@gmail.com>
2025-03-28 13:16:26 -0400

    blog client issue

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,189 +1,7 @@
-'use client';
-
-import { MDXRemote } from 'next-mdx-remote/rsc';
 import { getPostBySlug, getAllPosts } from '../../../utils/blog';
 import BlogLayout from '../../../components/blog/BlogLayout';
-import { mdxComponents } from '../../../components/mdx/MDXComponents';
-import { Typography, Box, Container } from '@mui/material';
-import Image from 'next/image';
-import { useEffect } from 'react';
-import { trackContentView } from '../../lib/analytics';
-
-const components = {
-  ...mdxComponents,
-  h1: (props) => (
-    <Typography variant="h1" component="h1" gutterBottom {...props} />
-  ),
-  h2: (props) => (
-    <Typography variant="h2" component="h2" gutterBottom {...props} />
-  ),
-  h3: (props) => (
-    <Typography variant="h3" component="h3" gutterBottom {...props} />
-  ),
-  h4: (props) => (
-    <Typography variant="h4" component="h4" gutterBottom {...props} />
-  ),
-  h5: (props) => (
-    <Typography variant="h5" component="h5" gutterBottom {...props} />
-  ),
-  h6: (props) => (
-    <Typography variant="h6" component="h6" gutterBottom {...props} />
-  ),
-  p: (props) => (
-    <Typography variant="body1" component="p" paragraph {...props} />
-  ),
-  ul: (props) => (
-    <Typography component="ul" sx={{ pl: 4, mb: 2 }} {...props} />
-  ),
-  ol: (props) => (
-    <Typography component="ol" sx={{ pl: 4, mb: 2 }} {...props} />
-  ),
-  li: (props) => (
-    <Typography component="li" sx={{ mb: 1 }} {...props} />
-  ),
-  blockquote: (props) => (
-    <Typography
-      component="blockquote"
-      sx={{
-        borderLeft: 4,
-        borderColor: 'primary.main',
-        pl: 2,
-        py: 1,
-        my: 2,
-        fontStyle: 'italic',
-      }}
-      {...props}
-    />
-  ),
-  code: (props) => (
-    <Typography
-      component="code"
-      sx={{
-        bgcolor: {
-          light: 'grey.100',
-          dark: 'grey.900'
-        },
-        color: {
-          light: 'grey.900',
-          dark: 'grey.300'
-        },
-        p: 0.5,
-        borderRadius: 1,
-        fontFamily: 'monospace',
-      }}
-      {...props}
-    />
-  ),
-  pre: (props) => (
-    <Typography
-      component="pre"
-      sx={{
-        bgcolor: {
-          light: 'grey.100',
-          dark: 'grey.900'
-        },
-        color: {
-          light: 'grey.900',
-          dark: 'grey.300'
-        },
-        p: 2,
-        borderRadius: 1,
-        overflow: 'auto',
-        my: 2,
-        '& code': {
-          bgcolor: 'transparent',
-          color: 'inherit',
-          p: 0,
-        }
-      }}
-      {...props}
-    />
-  ),
-  img: (props) => {
-    // Clean up the src path
-    let src = props.src || '';
-    
-    // Handle markdown image syntax [path]
-    if (src.startsWith('[') && src.endsWith(']')) {
-      src = src.slice(1, -1);
-    }
-    
-    // Handle relative paths
-    if (src.startsWith('./')) {
-      src = src.substring(2);
-    }
-    
-    // Handle absolute paths starting with /
-    if (src.startsWith('/')) {
-      src = src.substring(1);
-    }
-
-    // For debugging
-    console.log('Image src:', src);
-
-    return (
-      <Box
-        component="figure"
-        sx={{
-          my: 4,
-          mx: 0,
-          p: 0,
-          '&::before, &::after': {
-            content: '""',
-            display: 'table',
-          },
-          '&::after': {
-            clear: 'both',
-          },
-        }}
-      >
-        <Box
-          sx={{
-            position: 'relative',
-            width: '100%',
-            borderRadius: 1,
-            overflow: 'hidden',
-            boxShadow: 1,
-          }}
-        >
-          <Image
-            {...props}
-            src={`/${src}`}
-            alt={props.alt || ''}
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{
-              width: '100%',
-              height: 'auto',
-            }}
-            priority={true}
-          />
-        </Box>
-        {props.alt && (
-          <Box
-            component="figcaption"
-            sx={{
-              textAlign: 'center',
-              mt: 1,
-              color: 'text.secondary',
-              typography: 'caption',
-            }}
-          >
-            {props.alt}
-          </Box>
-        )}
-      </Box>
-    );
-  },
-  a: (props) => (
-    <Typography
-      component="a"
-      sx={{ color: 'primary.main', textDecoration: 'none' }}
-      {...props}
-    />
-  ),
-};
+import BlogPostContent from '../../../components/blog/BlogPostContent';
+import { Typography, Container } from '@mui/material';
 
 export async function generateStaticParams() {
   const posts = await getAllPosts();
@@ -259,11 +77,6 @@ export default async function BlogPost({ params }) {
     );
   }
 
-  // Track blog post view
-  useEffect(() => {
-    trackContentView('blog', params.slug, post.title);
-  }, [params.slug, post.title]);
-
   return (
     <BlogLayout
       title={post.title}
@@ -272,9 +85,7 @@ export default async function BlogPost({ params }) {
       date={post.date}
       author={post.author}
     >
-      <Box sx={{ mt: 4 }}>
-        <MDXRemote source={post.content} components={components} />
-      </Box>
+      <BlogPostContent post={post} slug={params.slug} />
     </BlogLayout>
   );
 } 
